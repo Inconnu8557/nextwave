9f85841bc1fda7b836026e43f88edebc59157794
AlessCreator <alessandro.m1@tumo.world>
2025-03-05 13:46:57 +0100

    initialize project structure with TypeScript, Vite, and essential components

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,12 @@ import { Navbar } from './components/Navbar'
 function App() {
 
   return (
-    <div>
+    <div className='min-h-screen bg-black text-gray-100 transition-opacity duration-700 pt-20'>
       <Navbar/>
-      <div>
+      <div className='container mx-auto px-4 py-4'>
           <Routes>
             <Route path='/' element={<Home/>}/>
+            <Route path='/' element={<></>}/>
           </Routes>
       </div>
     </div>
---
+++ src/pages/CreatePostPages.tsx
@@ -0,0 +1,5 @@
+export const CreatePostPages = () => {
+    return <div>
+        
+    </div>
+}
