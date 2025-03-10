ba01c27d90c2b8e0721cb292064c1fa5481b9fd6
AlessCreator <alessandro.m1@tumo.world>
2025-03-10 19:07:17 +0100

    implement LikeButton component with voting functionality

--- src/components/LikeButton.tsx
+++ src/components/LikeButton.tsx
@@ -1,12 +1,50 @@
+import { useMutation } from "@tanstack/react-query";
+import { supabase } from "../supabase-client";
+import { useAuth } from "../context/AuthContext";
+
 interface Props {
   postId: number;
 }
 
-export const LikeButton = ({}: Props) => {
+const vote = async (VoteValue: number, postId: number, userId: string) => {
+  const { data: existingVote } = await supabase
+    .from("votes")
+    .select("*")
+    .eq("posts_id", postId)
+    .eq("user_id", userId)
+    .maybeSingle();
+
+  if (existingVote) {
+    if (existingVote.vote === VoteValue) {
+      const { error } = await supabase
+        .from("votes")
+        .delete()
+        .eq("id", existingVote.id);
+
+        if (error) throw new Error(error.message);
+    }
+  }
+
+  const { error } = await supabase
+    .from("votes")
+    .insert({ post_id: postId, user_id: userId, vote: VoteValue });
+  if (error) throw new Error(error.message);
+};
+
+export const LikeButton = ({ postId }: Props) => {
+  const { user } = useAuth();
+
+  const { mutate } = useMutation({
+    mutationFn: (VoteValue: number) => {
+      if (!user) throw new Error("You must be logged in to vote");
+      return vote(VoteValue, postId, user.id);
+    },
+  });
+
   return (
     <div>
-      <button>Like</button>
-      <button>Dislike</button>
+      <button onClick={() => mutate(1)}>👍</button>
+      <button onClick={() => mutate(-1)}>👎</button>
     </div>
   );
 };
