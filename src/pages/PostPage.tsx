882d01f0641983d783340b5d3d67b6f1caa9457e
AlessCreator <alessandro.m1@tumo.world>
2025-03-25 19:30:35 +0100

    Ajoute une redirection automatique vers la page d'accueil dans le composant PostPage lors du chargement, en utilisant le hook useEffect et useNavigate de react-router-dom.

--- src/pages/PostPage.tsx
+++ src/pages/PostPage.tsx
@@ -1,8 +1,17 @@
-import { useParams } from "react-router";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
 import { PostDetail } from "../components/PostDetail";
+import { useNavigate } from 'react-router-dom';
 
 export const PostPage = () => {
   const { id } = useParams<{ id: string }>();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // Redirige vers la page d'accueil si la page est rechargée
+    navigate('/');
+  }, [navigate]);
+
   return (
     <div className="pt-10">
       <div>
