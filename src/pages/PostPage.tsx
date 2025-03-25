a69f3bf421b38881a0eba7aa9c65c9b270aa890d
AlessCreator <alessandro.m1@tumo.world>
2025-03-25 19:39:08 +0100

    Ajoute la récupération des détails d'un post dans le composant PostPage en utilisant le hook useQuery de react-query. Redirige vers la page d'accueil si le post n'est pas trouvé ou en cas d'erreur.

--- src/pages/PostPage.tsx
+++ src/pages/PostPage.tsx
@@ -1,21 +1,46 @@
 import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { PostDetail } from "../components/PostDetail";
-import { useNavigate } from 'react-router-dom';
+import { useQuery } from "@tanstack/react-query";
+import { supabase } from "../supabase-client";
+import { Post } from "../components/PostList";
 
 export const PostPage = () => {
   const { id } = useParams<{ id: string }>();
   const navigate = useNavigate();
 
+  const { data: post, error } = useQuery<Post, Error>({
+    queryKey: ['post', id],
+    queryFn: async () => {
+      if (!id) {
+        throw new Error("Post ID is undefined");
+      }
+      
+      const { data, error } = await supabase
+        .from('posts')
+        .select('*')
+        .eq('id', id)
+        .single();
+
+      if (error) {
+        console.error("Erreur lors de la récupération du post:", error.message);
+        throw error;
+      }
+      return data as Post;
+    },
+    enabled: !!id,
+  });
+
   useEffect(() => {
-    // Redirige vers la page d'accueil si la page est rechargée
-    navigate('/');
-  }, [navigate]);
+    if (!post && error) {
+      navigate('/');
+    }
+  }, [post, error, navigate]);
 
   return (
     <div className="pt-10">
       <div>
-        <PostDetail postId={Number(id)}/>
+        <PostDetail postId={Number(id)} />
       </div>
     </div>
   );
