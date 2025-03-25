8f54917a9dc92a028a15e2bff189343959a7153d
AlessCreator <alessandro.m1@tumo.world>
2025-03-25 19:26:20 +0100

    Ajoute la dépendance "react-router-dom" à package.json et package-lock.json. Met à jour le composant App pour utiliser "useNavigate" afin de rediriger vers la page d'accueil lors du chargement. Supprime une parenthèse inutile dans le composant Navbar.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -144,7 +144,6 @@ export const Navbar = () => {
                     </button>
                   </div>
                 )}
-                )
               </div>
             ) : (
               <button
