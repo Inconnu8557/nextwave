2260242a38c56b4995b6c8320af3da77c1da6e98
AlessCreator <alessandro.m1@tumo.world>
2025-05-18 19:23:03 +0200

    fix: correct formatting and improve readability in Home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { PostList } from "../components/PostList";
-import 'ldrs/react/Ring.css'
+import "ldrs/react/Ring.css";
 import { Ring } from "ldrs/react";
 
 export const Home = () => {
@@ -14,15 +14,19 @@ export const Home = () => {
 
     return () => clearTimeout(timer);
   }, []);
-
+  <script
+    src="//code.tidio.co/z0mrfrbm9byoa4dnl7mrpm6ov1j18a1c.js"
+    async
+  ></script>;
   return (
     <div className="pt-10">
       {loading ? (
-        <div className="pt-20 text-3xl text-center">Chargement... <br /><br /><br /><Ring
-        size="100"
-        speed="3"
-        color="purple" 
-      /></div> // Loader
+        <div className="pt-20 text-3xl text-center">
+          Chargement... <br />
+          <br />
+          <br />
+          <Ring size="100" speed="3" color="purple" />
+        </div> // Loader
       ) : (
         <>
           <h2 className="pt-12 mb-6 text-6xl font-bold text-center text-transparent bg-gradient-to-r from-purple-500 to-pink-500 bg-clip-text">
